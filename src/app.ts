15e9bb08f1c838aea2acb8aeb35c8deb8f41614c
Your Full Name <harshal@mamotechnolabs.com>
2026-07-06 21:53:36 +0530

    add error handing

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
-import express, { type Request, type Response } from 'express';
+import express, { type Request, type Response, type NextFunction } from 'express';
+import logger from './config/logger.js';
+import { HttpError } from 'http-errors';
 
 const app = express();
 
 app.get('/health', (req: Request, res: Response) => {
   res.status(200).json({
     msg: 'Server is running',
+  });
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: '',
+        location: '',
+      },
+    ],
   });
 });
 
